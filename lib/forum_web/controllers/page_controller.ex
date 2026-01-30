ea4144ce3edf08376c91a0056e80e14e3417b3e6
grey300 <tgyel3002@gmail.com>
2026-01-30 11:16:54 +0600

    learnt the basic structures and templatig engine

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -4,4 +4,16 @@ defmodule ForumWeb.PageController do
   def home(conn, _params) do
     render(conn, :home)
   end
+
+  def users(conn, _params) do
+    IO.puts("User hit the fucntion!")
+
+    users = [
+      %{id: 1, name: "Pema", age: 21},
+      %{id: 2, name: "Yams", age: 20},
+      %{id: 3, name: "Chims", age: 19}
+    ]
+
+    json(conn, %{users: users})
+  end
 end
